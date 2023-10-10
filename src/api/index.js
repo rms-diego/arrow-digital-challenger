394c0f34348c34564295ec026bbd64d504ffe54f
Diego Ramos <diegorbrito9@gmail.com>
2023-10-10 14:19:37 -0300

    refactor: renomeando o nome da pasta

--- src/api/index.js
+++ src/api/index.js
@@ -2,21 +2,21 @@
 api/index.js mounts all routes inside the api folder
 these routes are mounted in ../server.js on the route '/api'
 */
-import { Router } from "express";
-import models from "./models/index.js";
-import auth from "./modules/auth/index.js";
-import docs from "./docs/index.js";
+import { Router } from 'express'
+import models from './routes/index.js'
+import auth from './modules/auth/index.js'
+import docs from './docs/index.js'
 
-const router = Router();
+const router = Router()
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.json({
-    message: "API is working",
-  });
-});
+    message: 'API is working',
+  })
+})
 
-router.use("/models", models);
-router.use("/auth", auth);
-router.use("/docs", docs);
+router.use('/models', models)
+router.use('/auth', auth)
+router.use('/docs', docs)
 
-export default router;
+export default router
